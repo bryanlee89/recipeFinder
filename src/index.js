6a23a9f0348bc6f50f5abfaf3a2b3f9c57e340d2
bryanlee89 <minjae8989@gmail.com>
2017-08-29 16:09:07 -0700

    Route path modified

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
       <Switch>
-        <Route exact path='/' component={App} />
+        <Route exact path={process.env.PUBLIC_URL + '/'} component={App} />
         <Route path='/favorites' component={FavoriteRecipeList} />
       </Switch>
     </BrowserRouter>
